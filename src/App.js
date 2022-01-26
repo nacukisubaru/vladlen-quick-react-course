c6f82861aff094c943122914f28a2c7f8943e47b
nacukisubaru <alexitsheff@gmail.com>
2022-01-26 21:56:16 +0300

    возможность зачеркивать элементы

--- src/App.js
+++ src/App.js
@@ -2,16 +2,28 @@ import React from 'react';
 import TodoList from './Todo/TodoList';
 
 function App() {
-  const todos = [
+  const [todos, setTodos] = React.useState([
     {id:1, completed: false, title: 'Купить хлеб'},
-    {id:2, completed: false, title: 'Купить водку'},
+    {id:2, completed: true, title: 'Купить водку'},
     {id:3, completed: false, title: 'Купить молоко'},
-  ]
+  ]);
+  
+  function toggleTodo(id) {
+    setTodos(
+      todos.map(todo => {
+        if(todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo
+      })
+    )
+    console.log('todo id', todos)
+  }
 
   return(
     <div className="wrapper">
       <h1>React приложение</h1>
-      <TodoList todos={todos} />
+      <TodoList todos={todos} onToggle={toggleTodo} />
     </div>
   );
 }
